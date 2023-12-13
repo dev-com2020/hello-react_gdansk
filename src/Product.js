db0707e9e87df78387a98b92af1f4debabdf5f22
DEV-COM <kwp@tlen.pl>
2023-12-13 14:46:36 +0100

    14:46

--- src/Product.js
+++ src/Product.js
@@ -2,9 +2,14 @@ import React from "react";
 
 
 class Product extends React.Component {
+
+    showPrice(){
+        alert(this.props.item.price);
+    }
+
     render() {
         return <li>
-            <h3>{this.props.item.name}</h3>
+            <h3 onClick={() => this.showPrice()}>{this.props.item.name}</h3>
             <p>{this.props.item.description}</p>
         </li>;
     }
