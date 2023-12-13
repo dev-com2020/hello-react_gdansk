84c9541f50a39914bca8f2a3dcf774304a3d1dc4
DEV-COM <kwp@tlen.pl>
2023-12-13 12:49:59 +0100

    12:49

---
+++ src/Product.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+class Product extends React.Component {
+    render() {
+        return <li>
+            <h3>Product name</h3>
+            <p>Product description</p>
+        </li>;
+    }
+}
+export default Product;
