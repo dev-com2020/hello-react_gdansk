f4529625b1544f3ab5a47354cc5e9068ad9a6eab
DEV-COM <kwp@tlen.pl>
2023-12-13 12:16:03 +0100

    12:15

--- src/Catalog.js
+++ src/Catalog.js
@@ -1,8 +1,26 @@
 import React from "react";
+import "./Catalog.css";
+import obraz from "./image.png";
+
+// function Catalog() {
+//     return (
+//         <div>
+//             <h2>KATALOG</h2>
+//             <div><img src={obraz} alt="coś"></img></div>
+//         </div>
+        
+//     );
+//   }
 
 class Catalog extends React.Component {
     render() {
-        return <div><h2>KATALOG</h2></div>
+        let title = "Stan katalogu na dzień " + new Date().toDateString();
+        return (
+            <div>
+                <h2>{title}</h2>
+                    <div><img src={obraz} alt="coś"></img></div>
+            </div>
+        );
     }
 }
 
---
+++ src/Message.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Message extends React.Component {
+    render() {
+        let message;
+        let today = new Date().getDay();
+    if (today === 0) {
+        message = <div className="sorry">W niedzielę mamy wolne...</div>;
+    } else {
+        message = <div className="happy">W czym możemy pomóc...</div>;
+    }
+        return message;
+    }
+}
+
+export default Message;
